f4885058663f3fc740a010267304f76575e52406
shigamete <daiki_02@ymail.ne.jp>
2022-03-17 00:17:30 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:create, :new,]
-  
+  resources :rooms, only: [:create, :new,] do
+    resources :messages, only: [:index, :create]
+  end
 end
